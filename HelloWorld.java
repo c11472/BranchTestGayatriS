c70b8fb71a80d8bac3c38c0aed291ac654d9f3f2
Gayatri Mishra <gayatrimis1@gmail.com>
2023-04-04 17:31:22 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,9 @@
-class HelloWorld{
-public static void main(String args[]) {System.out.println("Hello world");}}
+class HelloWorld
+{
+public static void main(String args[])
+{
+  System.out.println("Hello world");
+                                       
+}
+}
 
